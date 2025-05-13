af8a0e7a5f3ca31696ce84eb53ac9cf0d7f9ab60
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2025-05-13 13:10:40 +0200

    redis

--- src/main/java/put/poznan/tsdcache/authentication/EmbeddedBadLoginAttemptsStorage.java
+++ src/main/java/put/poznan/tsdcache/authentication/EmbeddedBadLoginAttemptsStorage.java
@@ -1,30 +1,34 @@
 package put.poznan.tsdcache.authentication;
 
+import java.util.HashMap;
+
 public class EmbeddedBadLoginAttemptsStorage implements BadLoginAttemptsStorage {
 
     // TODO 1.2 - Create and initialize a field of HashMap type to store data
 
+    private HashMap<String, Integer> data = new HashMap<>();
+
     // TODO 1.2 - Finish this method
     @Override
     public Integer get(String key) {
-        return 0;
+        return data.getOrDefault(key,0);
     }
 
     // TODO 1.2 - Finish this method
     @Override
     public void put(String key, Integer value) {
-
+        data.put(key, value);
     }
 
     // TODO 1.2 - Finish this method
     @Override
     public void increment(String key) {
-
+        data.put(key, this.get(key) + 1);
     }
 
     // TODO 1.2 - Finish this method
     @Override
     public void remove(String key) {
-
+        data.remove(key);
     }
 }
